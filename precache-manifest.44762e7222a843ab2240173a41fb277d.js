36ddcaf295105b75a361590d516ee160378ff953
akademia108 <kursy@akademia108.pl>
2020-06-06 16:35:07 +0200

    Updates

---
+++ precache-manifest.44762e7222a843ab2240173a41fb277d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f715a099e76c6b80b20b2f2814f0dadc",
+    "url": "/react-example/index.html"
+  },
+  {
+    "revision": "0a72bb3f5f9ed26bae8c",
+    "url": "/react-example/static/css/main.f0b5752a.chunk.css"
+  },
+  {
+    "revision": "7dd0288dac562ada2083",
+    "url": "/react-example/static/js/2.98aa46ef.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-example/static/js/2.98aa46ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a72bb3f5f9ed26bae8c",
+    "url": "/react-example/static/js/main.35217438.chunk.js"
+  },
+  {
+    "revision": "91a7da182e845a375242",
+    "url": "/react-example/static/js/runtime-main.73f734a8.js"
+  }
+]);
